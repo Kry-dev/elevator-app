704dc9993710abb964b67b5ed30c67918bb860a5
Kry-dev <yaroslavkryvda@gmail.com>
2017-08-17 15:58:44 +0300

    create services page

--- src/js/modules/main.js
+++ src/js/modules/main.js
@@ -1,7 +1,7 @@
 export default {
   init() {
     const projectName = 'Elevator-app';
-    this.showMessage(`${projectName} works!`);
+    this.showMessage(`${projectName} works nice!`);
   },
   showMessage(message) {
     console.log(message);
